875230bf88c2eb7d239ce89b0b4514088970c2c2
PhillipKP <PhillipKP@yahoo.com>
2015-08-07 23:44:12 -0400

    Changes to be committed: 	modified:   problem3pt19.py

--- problem3pt19.py
+++ problem3pt19.py
@@ -1,4 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import 
+noiseTrials = 600000
+
+t = np.random.randint(1,7,noiseTrials)
+print(t)
+hist = np.bincount(t)
+print(hist/noiseTrials)
+
